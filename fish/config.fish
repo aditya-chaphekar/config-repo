0d62a01ad78c23807043b298c118a548de351ddb
Aditya <adityachaps@gmail.com>
2025-01-01 16:48:55 +0530

    tmux config changes

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,10 @@ if status is-interactive
     set PATH $GOROOT/bin:$GOPATH/bin $PATH
     set PATH $GOPATH/bin $PATH
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/tr4cer/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
---
+++ fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,5 @@
+function fish_user_key_bindings
+    bind --erase --key \el
+    bind --erase --key \eh
+    # and so on
+end
